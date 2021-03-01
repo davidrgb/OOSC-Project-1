95531469c13e79d957006f9f5cafd46b38b95bd3
davidrgb <davidrussellrgb@gmail.com>
2021-03-01 16:54:57 -0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocation(400, 100);
+
+        window.pack();
+        window.setVisible(true);
+    }
+}
