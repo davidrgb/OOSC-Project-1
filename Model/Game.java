36f8cbf54c43c3dba187303c22cd32a59e35ed7f
davidrgb <davidrussellrgb@gmail.com>
2021-03-01 18:02:29 -0600

    Created Game with wordPool

---
+++ Model/Game.java
@@ -0,0 +1,57 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Game {
+
+    private ArrayList<String> wordPool = new ArrayList<String>();
+    
+    private String target;
+    private String input;
+
+    private int health;
+    private int remainingLetters;
+
+    public Game() {
+        generateTarget();
+    }
+
+    public void generateTarget() {
+        if (true) {
+            wordPool.add("communication");
+            wordPool.add("science");
+            wordPool.add("programming");
+            wordPool.add("language");
+            wordPool.add("difficulty");
+            wordPool.add("artificial");
+            wordPool.add("intelligence");
+            wordPool.add("attempts");
+            wordPool.add("screenshot");
+            wordPool.add("baseball");
+            wordPool.add("windows");
+            wordPool.add("learning");
+            wordPool.add("electronics");
+            wordPool.add("beautiful");
+            wordPool.add("internet");
+            wordPool.add("database");
+            wordPool.add("organization");
+            wordPool.add("application");
+            wordPool.add("network");
+            wordPool.add("friendly");
+            wordPool.add("validation");
+            wordPool.add("attempts");
+            wordPool.add("statistics");
+            wordPool.add("physics");
+            wordPool.add("chemistry");
+            wordPool.add("engineering");
+            wordPool.add("school");
+            wordPool.add("industry");
+            wordPool.add("revolution");
+            wordPool.add("progress");
+            wordPool.add("characters");
+            wordPool.add("heavily");
+            wordPool.add("graphics");
+        }
+    }
+}
